8fe8470f192b7c0cadf8896bbbfc889fc95ba623
sayi <sayikrishnapk@gmail.com>
2024-10-30 21:14:53 +0530

    first comit

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ from cl_calculator import verify_input
 
 class Test_cli_input(unittest.TestCase):
     def test_input(self):
-        self.assertFalse(verify_input("import os rm -rf"))
         self.assertTrue(verify_input("7+8+1979767564463232098097986764453"))
         self.assertTrue(verify_input("99**8+1979767564463232098097986764453"))
         self.assertFalse(verify_input("(h+165),(),//@3%7"))
